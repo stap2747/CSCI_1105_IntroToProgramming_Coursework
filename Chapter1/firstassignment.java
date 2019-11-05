8a7daf290fdd30557a5ecdae49f3113ee47c033c
Shawn Stapel <57200400+stap2747@users.noreply.github.com>
2019-11-04 19:10:12 -0700

    Add assignment 1

---
+++ Chapter1/firstassignment.java
@@ -0,0 +1,13 @@
+/*
+ * Shawn Stapel
+ * 11/04/2019
+ * Exercise 1-5 
+ * Computes the equation given in the assignment
+ */
+
+public class firstassignment {
+	public static void main(String[] args) {
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+
+}
