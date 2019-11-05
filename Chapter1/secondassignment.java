3f72354d64514d37f823eed7ee6260bb8c6a0937
Shawn Stapel <57200400+stap2747@users.noreply.github.com>
2019-11-04 19:29:28 -0700

    uploading second assignment 1-9

---
+++ Chapter1/secondassignment.java
@@ -0,0 +1,13 @@
+/*
+ * Shawn Stapel
+ * Exercise 1-9
+ * 11/04/2019
+ * computes area and perimeter of a rectangle
+ */
+public class secondassignment {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println((4.5 * 2) +(7.9 * 2));
+	}
+
+}
