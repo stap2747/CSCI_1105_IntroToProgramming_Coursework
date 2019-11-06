372e466602a59a264417c8cded8ab91e718c9864
Shawn Stapel <57200400+stap2747@users.noreply.github.com>
2019-11-05 18:31:08 -0700

    third assignment

---
+++ Chapter1/thirdassignment.java
@@ -0,0 +1,12 @@
+/*
+ * Shawn Stapel
+ * 11/04/2019
+ * Exercise 1-13
+ */
+public class thirdassignment {
+	public static void main(String[] args) {
+		System.out.println(((44.5 * .55) - (50.2 * 5.9)) / ((3.4 * .55) - (50.2 * 2.1)));
+		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * .55) - (50.2 * 2.1)));
+	}
+
+}
